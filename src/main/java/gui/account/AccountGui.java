52e5a422ce575c4cc30113ccf6491a8a683ccc1b
Matt <mzuk0493@gmail.com>
2020-11-28 12:01:30 +0100

    create all controllers

---
+++ src/main/java/gui/account/AccountGui.java
@@ -0,0 +1,58 @@
+package gui.account;
+
+import data_base.account_information.AccountInformationFetcher;
+import model.domain.account.CurrentAccountBalance;
+
+import javax.swing.*;
+
+public class AccountGui {
+
+    public void createAccountGui(int id) {
+        CurrentAccountBalance currentAccountBalance = new CurrentAccountBalance();
+        JFrame frame = new JFrame();
+        JPanel mainPanel = new JPanel();
+
+        JLabel cash = new JLabel(currentAccountBalance.getCurrentAccountBalance(id).toString());
+        JLabel numberAccount = new JLabel("Your account number : ");
+        AccountInformationFetcher accountInformationFetcher = new AccountInformationFetcher();
+        JLabel accountNumber = new JLabel(accountInformationFetcher.getUserAccountNumber(id));
+        JLabel cashOnAccount = new JLabel("Cash on your account: ");
+
+        JButton transferButton = new JButton("transfer");
+        transferButton.addActionListener(actionEvent -> {
+            TransferGui transferGui = new TransferGui(currentAccountBalance.getCurrentAccountBalance(id), id, cash);
+            transferGui.show();
+        });
+
+        JButton creditCardButton = new JButton("use card");
+        creditCardButton.addActionListener(actionEvent -> {
+            CreditCardGui creditCardGui = new CreditCardGui(currentAccountBalance.getCurrentAccountBalance(id), id, cash);
+            creditCardGui.show();
+        });
+
+        JButton depositButton = new JButton("deposit");
+        depositButton.addActionListener(actionEvent -> {
+            DepositGui depositGui = new DepositGui(id, cash);
+            depositGui.show();
+        });
+
+        JButton checkTransferHistoryButton = new JButton("check History");
+        checkTransferHistoryButton.addActionListener(actionEvent -> {
+            HistoryGui gui = new HistoryGui(id);
+            gui.createTable();
+        });
+
+        mainPanel.add(cashOnAccount);
+        mainPanel.add(cash);
+        mainPanel.add(numberAccount);
+        mainPanel.add(accountNumber);
+        mainPanel.add(transferButton);
+        mainPanel.add(creditCardButton);
+        mainPanel.add(depositButton);
+        mainPanel.add(checkTransferHistoryButton);
+
+        frame.getContentPane().add(mainPanel);
+        frame.setSize(280, 300);
+        frame.setVisible(true);
+    }
+}
